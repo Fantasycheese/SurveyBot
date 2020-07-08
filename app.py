e2fb93888bec6b6ba55bcf9cbd65bdd1723465b7
Joshua Lin <joshua@ampostech.com>
2020-07-08 17:37:43 +0800

    test Dialogflow webhook

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
+import json
+
 from flask import Flask
 from flask import request
 
 app = Flask(__name__)
 
+sessions = {}
+questions = [
+    "Do you use GR in the meetings?",
+    "What are those meetings? Can you share more details?",
+    "What are the interaction you do with GR in the meetings? (update data?, check and sync progress? for future planning?...)"
+    "Does GR help?"
+    "Why?"
+]
+
 
 @app.route('/')
 def home():
     return 'Hello Flask'
+
+
+@app.route('/get-next-question')
+def get_next_question():
+    body = request.get_json()
+    session = body["session"]
+    if session not in sessions:
+        sessions[session] = 5
+    question = questions[sessions[session]]
+    sessions[session] -= 1
+    return json.dumps({
+        "fulfillmentMessages": [
+            {
+                "text": {
+                    "text": [
+                        question
+                    ]
+                }
+            }
+        ]
+    })
